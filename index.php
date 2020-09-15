9815a590d2bc933db6945a552a64940b0231b35a
SrKalikar <33619493+SrKalikar@users.noreply.github.com>
2020-09-15 09:17:18 -0700

    files
    easy-project-files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "Students_Database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+//query formation within the table
+$sql = "INSERT INTO Students_Data (firstname, middlename, lastname, email, branch, year, roll_no)
+VALUES ('firstname', 'middlename', 'lastname', 'email', 'branch', 'year', 'roll_no')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
